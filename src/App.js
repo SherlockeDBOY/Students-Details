342e57b83c443b5720ce217efa9c90b64c4d8974
SherlockeDBOY <aswintherealone@gmail.com>
2021-07-03 14:28:50 +0530

    03-07-2021

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './css/inputbox.style.css'
 import {CardContainer} from './components/card-container/card-container.component';
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
     constructor(){
@@ -10,6 +11,8 @@ class App extends Component {
             students: [],
             searchField: ''
         };
+
+        // this.handleChange = this.handleChange.bind(this); (Need not to write this statement for an arrow function just for a normal js function)
     }
 
     componentDidMount() {
@@ -18,24 +21,24 @@ class App extends Component {
         .then(users => this.setState({students: users}));
     }
 
+    handleChange = event => {
+        this.setState({searchField: event.target.value});
+    };
+
     render() {
-        const { students, searchField } = this.state;
+        const { students, searchField } = this.state; //Destructing
         const filteredStudents = students.filter( student =>
             student.name.toLowerCase().includes(searchField.toLowerCase())
-        );
+        ); 
+        //.filter() will give a new array
+        //.includes() checks whether the string value passed inside the includes is actually in the student
 
         return (
             <div className='App'>
                 <h1 className="heading"> Student Details </h1>
-                <input 
-                    type="search" 
-                    placeholder="Search Students"
-                    onChange= {
-                        e => {
-                            this.setState({searchField: e.target.value})
-                        }
-                    }
-                    id="inputBox"
+                <SearchBox
+                placeholder = 'Search Students'
+                handleChange = {this.handleChange}
                 />
                 <CardContainer students={filteredStudents}/>
             </div>
